9befe12a5198feedcc24863e3dc3d6de351d00ca
umarmahir <umarmahir10@gmail.com>
2022-09-08 17:02:45 -0700

    Committing git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-/*Some little decc.*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	/*Some description*/
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
