fcdd9c01fbb2531e9c277e722854781e5be9c093
umar <umarmahir10@gmail.com>
2022-09-09 16:37:59 -0700

    Commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,20 @@
 int main(void)
 {
 	int i;
+	int j;
+	int k;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
+		for (j = 1; j <= 9; j++)
+		{
 		putchar(',');
+		}
+		for (k = 1; k <= 9; k++)
+		{
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
