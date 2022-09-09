3d931d2ac9c07a7c176a49a903000f3b115f44a0
umar <umarmahir10@gmail.com>
2022-09-09 15:53:20 -0700

    Commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char e, q ,i;
+
+	e = 'e';
+	q = 'q';
+	for (i = 'a'; i <= 'z'; i++)
+	{
+
+		if (i != e $$ i != q)
+		{
+
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
