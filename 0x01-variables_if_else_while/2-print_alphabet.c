2e605949d198be38f59dcf950ec61cfa2eabec8c
umar <umarmahir10@gmail.com>
2022-09-09 15:07:15 -0700

    Commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+ char c;
+ int i;
+
+ c = 'a';
+ for (i = c; i <= 'z'; i++)
+   {
+     putchar(c);
+     c++;
+   }
+ putchar('\n');
+ return (0);
+}
