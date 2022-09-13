6300083d5fd4e22b779153e67789ef243a263cd2
umar <umarmahir10@gmail.com>
2022-09-13 15:51:24 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Description: 'A program that prints alphabet in lowercase'
  *
  * Return: Always 0 (Success)
  */
-
-
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}
-
 
 void print_alphabet(void)
 {
