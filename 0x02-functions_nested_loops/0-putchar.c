580c36e40ae2aac6d6ba8a2c8b0abddc1ac6e928
umar <umarmahir10@gmail.com>
2022-09-13 14:42:54 -0700

    Committing changes

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'Printing characters using while statement'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main (void)
+{
+	char p = "_putchar";
+
+
+	while ( *p )
+	{
+		_putchar(p);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning about Functions and nested loop
