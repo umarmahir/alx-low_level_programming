affa8df7c859a59243f6babafa461db750e54aca
umar <umarmahir10@gmail.com>
2022-09-13 18:25:47 -0700

    Done for the day

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,12 +17,15 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			if (n != 98)
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
 			{
 				printf("%d, ", n);
 				n++;
 			}
-			printf("%d, ", n);
 		}
 	}
 	else if (n == 98)
@@ -33,12 +36,15 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			if (n != 98)
+			if (n == 98)
 			{
-				printf("%d, ", n);
-				n--;
+				printf("%d", n);
 			}
-			printf("%d", n);
+			else
+			{
+			printf("%d, ", n);
+			n--;
+			}
 		}
 	}
 	printf("\n");
