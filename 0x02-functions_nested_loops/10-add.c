54bd9912f751f1eec2e0b8ea668e7c137b71d3ca
umar <umarmahir10@gmail.com>
2022-09-13 17:41:53 -0700

    Thiswas quite difficult

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: 'A program that adds two numbers'
+ *
+ * Return: Always 0 (Success)
+ *
+ * @a: the first number
+ *
+ * @b: the second number
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
