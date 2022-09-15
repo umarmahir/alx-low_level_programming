1180fcae354ecdd83f4d07b075798c43bd4ef767
umar <umarmahir10@gmail.com>
2022-09-15 13:05:14 -0700

    Committing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
-#include "main.h"
 /**
- * print_numbers - Entry point
+ * print_most_numbers - Entry point
  *
  * Description: 'the program's description'
  *
  * Return: Always 0 (Success)
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		if (i != 2 || i != 4)
+		{
+			_putchar(i);
+		}
 	}
-	_putchar('\n');
 }
