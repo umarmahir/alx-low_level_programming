d247e49c3d228f65efb37ef8a510783bd85c3d1b
umar <umarmahir10@gmail.com>
2022-09-19 13:33:10 -0700

    Back from break

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,10 @@
 void print_rev(char *str)
 {
 	int length;
+	int i;
 
 	length = strlen(str);
-	for (int i = (length - 1); i >= 0; i++)
+	for (i = (length - 1); i >= 0; i++)
 	{
 		_putchar(str[i]);
 	}
