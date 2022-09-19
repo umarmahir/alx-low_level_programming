8d2640d31651e7e443d4915235f04b1985308454
umar <umarmahir10@gmail.com>
2022-09-19 08:29:02 -0700

    First Commit of the day

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * resest_to_98 - Entry point
+ *
+ * Description: 'Resets the value a pointer point at to 98'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98 (int *n)
+{
+	*n = 98;
+}
