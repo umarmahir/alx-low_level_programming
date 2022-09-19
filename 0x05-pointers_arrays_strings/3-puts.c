4413a984cc00ea2fe0f96e5c56c61a3da1a70a40
umar <umarmahir10@gmail.com>
2022-09-19 09:53:53 -0700

    Commiting

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+/**
+ * _puts - Entry point
+ *
+ * Description: 'The function that prints strings to the stdout'
+ *
+ * @str: The string to br displayed
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
