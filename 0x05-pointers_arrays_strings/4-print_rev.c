e4a485fec2630fd4cd7731a894710d7d52b4c4ef
umar <umarmahir10@gmail.com>
2022-09-19 10:19:42 -0700

    Commiting

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ *
+ * Description: 'The function that prints strings in reverse order'
+ *
+ * @str: The string to br displayed
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*str);
+		i--;
+		str--;
+	}
+	_putchar('\n');
+}
