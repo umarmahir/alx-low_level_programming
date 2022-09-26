43c78df840573d3ce147cc96ee0e57228f07a306
umar <umarmahir10@gmail.com>
2022-09-26 14:06:55 -0700

    Making first major commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
- * _memset - Entry point
- *
- * Description: 'A pgrm that sets the value of sets of memory locs. to a val'
- * @s: The pointer carrying the address
- * @b: The byte
- * @n: The number of places to changes
- * Return: Always 0 (Success)
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -18,19 +15,3 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
-/**
-char *_memset(char *s, char b, unsigned int n)
-{
-	char **pptr;
-	unsigned int i;
-
-	i = 0;
-	pptr = &s;
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
-	return (s);
-}
-*/
