a3b0453c31ba70c73c4b0f467a1d38cb6f4c6c4c
umar <umarmahir10@gmail.com>
2022-09-28 14:42:54 -0700

    Committing changes

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ *
+ * Description: 'A program that prints a string in reverse recursively'
+ * @s: The string to print
+ * Return: Always 0 (Success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(*s + 1);
+		_putchar(*s);
+	}
+}
