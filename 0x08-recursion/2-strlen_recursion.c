ac1b4d27f59b1f3ab59fe871ee857d861f726b5f
umar <umarmahir10@gmail.com>
2022-09-28 14:51:43 -0700

    Committing changes

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Entry point
+ *
+ * Description: 'A program that counts the number of chars in a string'
+ * @s: The string to count
+ * Return: Always 0 (Success)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
