2fd5fa4bc8be14ce559f2b0b458671d6367bbf1d
umar <umarmahir10@gmail.com>
2022-09-28 14:13:54 -0700

    Committing changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,14 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (*(s + i) == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar("%c", *(s + i));
+	_putchar("%c", *s);
 	_puts_recursion(s + 1);
 }
