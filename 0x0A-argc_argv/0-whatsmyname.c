16e260e768d6850ffed9dd6fdb6c2636125dc871
umar <umarmahir10@gmail.com>
2022-09-30 15:11:22 -0700

    Committing

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
 * Return: Always 0 (Success)
 */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-        printf(argv[0]);
+	printf("%s", argv[0]);
 	_putchar("\n");
 	return (0);
 }
