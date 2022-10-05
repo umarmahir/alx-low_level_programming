b3068a33be3e102f1af10ecef18f21fbd9030470
umar <umarmahir10@gmail.com>
2022-10-04 18:44:18 -0700

    Commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Entry point
+ *
+ * Description: 'A program that concatenates two strings'
+ * @s1: The first string
+ * @s2: Second string
+ * Return: Always 0 (Success)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	char *t;
+	int len;
+	int j;
+
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	t = malloc(sizeof(char) * (len1 + len2));
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (s1[i] != '\0')
+		{
+			t[i] = s1[i];
+			i++;
+		}
+		while (s2[j] != '\0')
+		{
+			t[i + 1 + j] = s2[j];
+			j++;
+		}
+		return (t);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,3 @@
+char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 int _putchar(char c);
