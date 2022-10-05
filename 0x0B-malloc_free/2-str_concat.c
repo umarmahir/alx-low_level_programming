b5cdfab66e0821b052b9cdeb1436f081a177764a
umar <umarmahir10@gmail.com>
2022-10-04 19:30:13 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,13 +22,13 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
 	t = malloc(sizeof(char) * (len1 + len2) + 1);
 	if (t == NULL)
 	{
