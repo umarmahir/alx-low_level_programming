9d72b6e01162fc59e54bab5dabbf5821b21e9587
umar <umarmahir10@gmail.com>
2022-10-04 19:47:32 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - Entry point
+ *
+ * Description: 'A program that retuens a pointer to 2d array of ints'
+ * @width: The width
+ * @height: Height
+ * Return: Always 0 (Success)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	char *t;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	t = malloc(sizeof(int) * width * height);
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			t[i][j] = 0;
+		}
+	}
+	return (t);
+}
