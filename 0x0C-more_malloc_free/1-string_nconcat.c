7c2ae7aa239a6c53deb182b46ad338bd4b549de5
Umar Murtala <umarmahir10@gmail.com>
2022-10-06 14:42:33 -0700

    Committing changes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= strlen(s2))
 		n = strlen(s2);
-	t = malloc(strlen(s1) + strlen(s2) + 1);
+	t = malloc(strlen(s1) + n + 1);
 	if (t == NULL)
 	{
 		return (NULL);
