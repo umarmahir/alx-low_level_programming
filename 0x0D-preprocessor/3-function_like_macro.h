42d857812fdec94b4f2500acc6195b9597dff6c9
Umar Murtala <umarmahir10@gmail.com>
2022-10-10 16:43:36 -0700

    Committing..

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(x) ((x) > (0) ? (x) : (x * -1))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef _SUM_X
+#define _SUM_X
 #define SUM(x, y)  (x + y)
 #endif
