7bf09fd0e3d6cc704c3df6df721dcda1d2838376
Umar Murtala <umarmahir10@gmail.com>
2022-10-10 15:04:36 -0700

    committing

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - The function to write
+ *
+ * Description: A function that prints a dog struct
+ *
+ * @d: The dog struct pointer
+ * Return: void
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age != 0.0)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+
+}
