53d17636ba0c21963b04a4007485bad4065ec975
Umar Murtala <umarmahir10@gmail.com>
2022-10-10 15:31:17 -0700

    committing

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,9 +17,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr;
 
 	ptr = &idog;
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
-		return(NULL);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
