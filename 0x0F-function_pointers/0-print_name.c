e1e5745ef3b0facc26c91d3613c73fe781cab862
Umar Murtala <umarmahir10@gmail.com>
2022-10-12 15:34:35 -0700

    Commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - Entry point
+ *
+ * Description: 'A program that prints name'
+ * @name: The name to print
+ * @f: pointer to a function
+ * Return: Always 0 (Success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
