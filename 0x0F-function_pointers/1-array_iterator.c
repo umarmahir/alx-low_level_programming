32eefb6601443e7305fd5e6bc2f8a35602902592
Umar Murtala <umarmahir10@gmail.com>
2022-10-12 16:02:42 -0700

    Alhamdulillah

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	for (i = 0; i < (int) size; i++)
+		action(*(array + i));
 }
