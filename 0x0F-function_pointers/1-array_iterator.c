87c8f99f373932f52a3a7e4bed6925534505b262
Umar Murtala <umarmahir10@gmail.com>
2022-10-12 15:54:39 -0700

    Alhamdulillah

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - Entry point
+ *
+ * Description: 'A program that performs action on elements of array'
+ * @array: The array containing elements
+ * @size: size of array
+ * @action: the pointer to function performing action
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
