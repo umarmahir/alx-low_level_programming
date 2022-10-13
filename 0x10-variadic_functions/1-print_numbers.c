3d76578ae6ac7dd0ee218919a63900ae51433b81
Umar Murtala <umarmahir10@gmail.com>
2022-10-13 15:25:03 -0700

    Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: 'A program that prints numbers'
+ * @n: The number of integers
+ * @separator: The separator to print in between numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if ((i != n - 1) && (separator != NULL))
+		{
+			printf("%s", separator);
+		}
+	}
+}
