d27a7a77dc2e967e62669da38a46f513b5e7e793
Umar Murtala <umarmahir10@gmail.com>
2022-10-12 17:08:37 -0700

    do

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,19 +15,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int j;
 
-	if (size <= 0)
-		return (-1);
-
-	if (cmp && array)
+	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			j = cmp(array[i]);
-			if (j != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
-
 	}
+
+	return (-1);
 }
