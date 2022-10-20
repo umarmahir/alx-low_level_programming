5addf7dd6aa34cdc6857ab234bcfdd8c209db3e7
Umar Murtala <umarmahir10@gmail.com>
2022-10-20 15:17:59 -0700

    Committing

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Entry point
+ *
+ * Description: A program that prints all the elements of a linked list
+ * @h: pointer the the list_t structure
+ * Return: Number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
